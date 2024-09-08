a541c54be3e01ef1796f08f4ec8a88aa7778d692
Selucus <bchapman141@gmail.com>
2024-09-08 20:24:30 +0100

    updates

--- unSupervisedTrading.py
+++ unSupervisedTrading.py
@@ -28,6 +28,57 @@ df = yf.download(tickers=symbols_list,
                  end=end_date)
 
 df = df.stack()
+
+
+
+df.index.names = ['date','ticker']
+df.columns = df.columns.str.lower()
+
+
+df['garman_klass_volatility'] = ((np.log(df['high'])-np.log(df['low']))**2)/2 - (2*np.log(2)-1)*((np.log(df['adj close'])) - np.log(df['open']))**2
+
+# group by ticker and calculate relative strength index
+df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
+
+# look at using other bolinger band levels
+df['bb_low'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
+
+df['bb_mid'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
+
+df['bb_high'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
+
+# using ATR - average true range
+
+def compute_atr(stock_data):
+    atr = pandas_ta.atr(high=stock_data['high'],
+                        low=stock_data['low'],
+                        close=stock_data['close'],
+                        length = 14)
+
+    #normalises
+    return atr.sub(atr.mean()).div(atr.std())
+
+df['atr'] = df.groupby(level=1, group_keys=False).apply(compute_atr)
+
+# moving average convergence / divergence
+def compute_macd(close):
+    macd = pandas_ta.macd(close=close,length=20).iloc[:,0]
+
+    #normalises for machine learning
+    return macd.sub(macd.mean()).div(macd.std())
+
+df['macd'] = df.groupby(level=1, group_keys=False)['adj close'].apply(compute_macd)
+
+
+# dollar volume in millions
+
+df['dollar_volume'] = (df['adj close']*df['volume'])/1e6
+
+
+
+
+
+
 with open('df.pkl','wb') as f:
     pickle.dump(df,f)
     f.close()
@@ -36,13 +87,45 @@ with open('df.pkl', 'rb') as f:
 
     df = pickle.load(f) # deserialize using load()
 
-df.index.names = ['date','ticker']
-df.columns = df.columns.str.lower()
+
+# to reduce training time with features and strategies we can aggregate to a monthly level 
+# and filter only top 150 most liquid stocks
+
+last_cols = [c for c in df.columns.unique(0) if c not in ['dollar_volume','volume','open','high','low','close']]
 
 
-df['garman_klass_volatility'] = ((np.log(df['high'])-np.log(df['low']))**2)/2 - (2*np.log(2)-1)*((np.log(df['adj close'])) - np.log(df['open']))**2
+data = (pd.concat([df.unstack('ticker')['dollar_volume'].resample('M').mean().stack('ticker').to_frame('dollar_volume'),
+           df.unstack()[last_cols].resample('M').last().stack('ticker')],
+           axis=1)).dropna()
 
-# group by ticker
-df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
-df.xs('AAPL',level=1)['rsi'].plot()
-print(df)
+# calculate 5-year rolling average of dollar volume for each stocks before filtering
+
+data['dollar-volume'] = data['dollar-volume'].unstack('ticker').rolling(5*12).mean().stack()
+
+data['dollar-vol-rank'] = data.groupby('date')['dollar-volume'].rank(ascending=False)
+
+data = data[data['dollar-vol-rank']<150].drop(['dollar-volume'],axis=1)
+
+
+# calculate monthly returns for different time horizons as features
+
+def calculate_returns(df):
+
+
+    outlier_cutoff = 0.005
+
+    lags = [1,2,3,6,9,12]
+
+    for lag in lags:
+        df[f'return_{lag}m'] = (g['adj close']
+                            .pct_change(lag)
+                            .pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),
+                                                                                    upper=x.quantile(1-outlier_cutoff)))
+                                .add(1)
+                                .pow(1/lag)
+                                .sub(1)
+                                )
+    return df
+
+
+data = data.groupby(level=1, group_keys=False).apply(calculate_returns).dropna()
